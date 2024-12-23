30a99226fb64be34809cff6ed83412f167ffe462
davidprocode <davidprocode@proton.me>
2024-12-23 13:39:54 -0300

    more updates without any progress

--- src/app/filehandler/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export function GET(res: NextApiResponse) {
-  res.status(200).json({ message: "Hello from API!" });
-}
-
-export function POST(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: "Hello from API!" });
-}
